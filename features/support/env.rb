70fc4066e2a0e5c3fd92a8cd4413b271e9a4514f
Illia Ferents <95219692+IlliaFerents@users.noreply.github.com>
2023-04-19 22:14:35 +0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,3 +4,7 @@ AllureCucumber.configure do |config|
   config.results_directory = 'results/allure-results'
   config.clean_results_directory = true # set 'false' to see runs trend
 end
+
+at_exit do
+  system('allure generate results/allure-results --clean')
+end
